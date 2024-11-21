a3ee9c564aa89a33b53d0c60d1e5b3c18fcb57cd
Brandon Ly <brandonly@lostcoding.com>
2024-11-21 12:21:23 -0500

    Add chumsky

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
+mod toml;
 mod utils;
 
+use chumsky::Parser;
+use toml::parser;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -10,4 +13,12 @@ extern "C" {
 #[wasm_bindgen]
 pub fn greet() {
     alert("Hello, toml-js!");
+}
+
+#[wasm_bindgen]
+pub fn parse() {
+    let parser = parser();
+    let result = parser.parse("5");
+
+    println!("{:?}", result)
 }
---
+++ src/toml.rs
@@ -0,0 +1,10 @@
+use chumsky::{prelude::*, Parser};
+
+#[derive(Clone, Debug)]
+pub enum Toml {
+    Num(f64),
+}
+
+pub fn parser() -> impl Parser<char, Toml, Error = Simple<char>> {
+    filter(|c: &char| c.is_ascii_digit()).map(|c| Toml::Num(c.to_digit(10).unwrap() as f64))
+}
